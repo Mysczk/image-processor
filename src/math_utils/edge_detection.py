12c8f4c9091505234fe0f655048e0918526aa122
Mysczk <michal.soukup.jr@gmail.com>
2025-05-08 14:30:43 +0200

    optimization

--- src/math_utils/edge_detection.py
+++ src/math_utils/edge_detection.py
@@ -1,8 +1,22 @@
 import numpy as np
 from PIL import Image
-from .convolution import apply_convolution
+from scipy import ndimage
 
 def detect_edges(image):
+    """
+    Detekuje hrany v obrázku pomocí Sobelových operátorů.
+    Používá optimalizované scipy.ndimage.convolve funkce.
+    
+    Args:
+        image: PIL Image objekt
+        
+    Returns:
+        PIL Image s detekovanými hranami
+    """
+    # Převedení na grayscale
+    image_array = np.array(image.convert("L"))
+    
+    # Definice Sobelových operátorů
     sobel_x = np.array([[-1, 0, 1],
                         [-2, 0, 2],
                         [-1, 0, 1]])
@@ -10,11 +24,13 @@ def detect_edges(image):
     sobel_y = np.array([[-1, -2, -1],
                         [0,   0,  0],
                         [1,   2,  1]])
-
-    gx = np.array(apply_convolution(image, sobel_x))
-    gy = np.array(apply_convolution(image, sobel_y))
-
+    
+    # Aplikace konvoluce přímo s využitím scipy
+    gx = ndimage.convolve(image_array, sobel_x, mode='constant', cval=0.0)
+    gy = ndimage.convolve(image_array, sobel_y, mode='constant', cval=0.0)
+    
+    # Výpočet magnitudy gradientu
     magnitude = np.sqrt(gx ** 2 + gy ** 2)
     magnitude = np.clip(magnitude, 0, 255).astype(np.uint8)
-
+    
     return Image.fromarray(magnitude)
